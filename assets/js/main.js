6d25b1bc42934edd46c151c9a1b784de36f561c8
Iconic Robbott <dev.robbott@gmail.com>
2025-06-10 14:38:44 +0100

    Added the "Why Choose Jamie?" section Added navbar fixed on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Mobike navigation
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menuToggle");
     const mobileMenu = document.querySelector(".mobile-nav-menu");
@@ -14,4 +15,23 @@ document.addEventListener("click", function (e) {
     if (!menu.contains(e.target) && !toggle.contains(e.target)) {
         menu.classList.remove("show");
     }
+});
+
+// Show menu on scroll
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        const nav = document.querySelector('nav');
+        const ctnBtn = document.getElementById('scrollBtn');
+        const scrollPoint = 500; // Scroll point
+
+        if (window.scrollY > scrollPoint) {
+            nav.classList.add('fixed');
+            ctnBtn.classList.add('btn-outline-white');
+            ctnBtn.classList.remove("btn-main");
+        } else {
+            nav.classList.remove('fixed');
+            ctnBtn.classList.remove("btn-outline-white");
+            ctnBtn.classList.add('btn-main');
+        }
+    });
 });
